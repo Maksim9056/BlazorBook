c7a88089cee1fcfe7be0116cc70bc16f968965eb
Maksim9056 <bobreczovm@inbox.ru>
2024-04-01 15:31:26 +0300

    Добавьте файлы проекта.

---
+++ BlazorBook/BlazorBook/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorBook.Models
+{
+    /// <summary>
+    /// Class for Book
+    /// </summary>
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+
+        public string Name { get; set; }
+        public string Year_of_publication { get; set; }
+        public Book(int id, string author, string name, string year_of_publication)
+        {
+            Id = id;
+            Author = author;
+            Name = name;
+            Year_of_publication = year_of_publication;
+        }
+
+        public Book()
+        {
+        }
+    }
+}
